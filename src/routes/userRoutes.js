54f6c56808c6ef662eb3b69e9be9542e4657ab9c
sivaganeshv1729 <sivaganeshv1729@gmail.com>
2026-07-16 00:32:08 +0530

    feat(api): add user routes

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,27 +1,12 @@
 const express = require('express');
-const Joi = require('joi');
-const validate = require('../middleware/validate');
+const router = express.Router();
+const UserController = require('../controllers/UserController');
+const { validateRequest, userSchema, userUpdateSchema } = require('../middleware/validation');
 
-const createUserSchema = Joi.object({
-    name: Joi.string().required(),
-    email: Joi.string().email().required()
-});
+router.post('/', validateRequest(userSchema), UserController.createUser);
+router.get('/:id', UserController.getUser);
+router.put('/:id', validateRequest(userUpdateSchema), UserController.updateUser);
+router.delete('/:id', UserController.deleteUser);
+router.get('/:id/enriched', UserController.getEnrichedUser);
 
-const updateUserSchema = Joi.object({
-    name: Joi.string(),
-    email: Joi.string().email()
-}).min(1);
-
-function setupUserRoutes(userController) {
-    const router = express.Router();
-
-    router.post('/', validate(createUserSchema), (req, res, next) => userController.createUser(req, res, next));
-    router.get('/:id', (req, res, next) => userController.getUser(req, res, next));
-    router.get('/:id/enriched', (req, res, next) => userController.getEnrichedUser(req, res, next));
-    router.put('/:id', validate(updateUserSchema), (req, res, next) => userController.updateUser(req, res, next));
-    router.delete('/:id', (req, res, next) => userController.deleteUser(req, res, next));
-
-    return router;
-}
-
-module.exports = setupUserRoutes;
+module.exports = router;

2cb6c6973932629ce7958a52b59575a0015db865
sivaganeshv1729 <sivaganeshv1729@gmail.com>
2026-07-16 00:32:05 +0530

    feat(repo): add IUserRepository interface

--- src/repositories/interfaces/IUserRepository.js
+++ src/repositories/interfaces/IUserRepository.js
@@ -4,7 +4,6 @@ class IUserRepository {
     async update(id, updates) { throw new Error("Not implemented"); }
     async delete(id) { throw new Error("Not implemented"); }
     async findByEmail(email) { throw new Error("Not implemented"); }
-    async findAll() { throw new Error("Not implemented"); }
 }
 
 module.exports = IUserRepository;
